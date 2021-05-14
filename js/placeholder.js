8a9c338998ce07a39222507dbea52dd6f4d94fe8
Andrey Emelin <andrey.v.emelin@gmail.com>
2021-05-14 19:06:23 +0300

    after desktop

---
+++ js/placeholder.js
@@ -0,0 +1,16 @@
+const formItems = document.querySelectorAll('.contact-form__field');
+
+for (let formItem of formItems) {
+    const placeholder = formItem.closest('.contact-form__item')
+        .querySelector('.contact-form__fake-placeholder');
+
+    formItem.addEventListener('focus', () => {
+        placeholder.classList.add('active');
+        formItem.classList.add('active');
+    });
+
+    formItem.addEventListener('blur', () => {
+        if (formItem.value.length == 0) { placeholder.classList.remove('active'); }
+        formItem.classList.remove('active');
+    });
+}
--- js/workFilter.js
+++ js/workFilter.js
@@ -1,17 +1,22 @@
-const filters = document.querySelectorAll('.filters__item');
+$(document).ready(function () {
 
-// Save currently shown element so as not to hide all elements every time
-let currentlyShown = filters[0];
+    const filters = document.querySelectorAll('.filters__item');
 
-// main part
-filters.forEach((value) => {
-    value.addEventListener('click', function () {
-        if (this != currentlyShown) {
-            currentlyShown.classList.toggle('filters__item--active');
-            this.classList.toggle('filters__item--active');
-            currentlyShown = this;
+    // Save currently shown element so as not to hide all elements every time
+    let currentlyShown = filters[0];
 
-            // Other actions for real filtering
-        };
+    // main part
+    filters.forEach((value) => {
+        value.addEventListener('click', function () {
+            if (this != currentlyShown) {
+                currentlyShown.classList.toggle('filters__item--active');
+                this.classList.toggle('filters__item--active');
+                currentlyShown = this;
+
+                // Other actions for real filtering
+            };
+        });
     });
-});
+
+}); // document.ready
+
